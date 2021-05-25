52b9a9e39479fb244f44b2cc9251936477380d64
Novuscoin <webmaster@makeitmoveitfx.com>
2021-05-24 20:22:20 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,6 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
+  "173.201.176.153:17236",
   "3.137.202.11:17236",
   "13.58.76.92:17236",
 };
